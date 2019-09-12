2ee0377766f9afd5c9a57da706498a0f06ad6926
Kiel Lewis <kiellewis@Kiels-MBP.hsd1.ga.comcast.net>
2019-09-12 13:49:17 -0400

    restared the whole project
    restarted

--- js/app.js
+++ js/app.js
@@ -1,314 +1,198 @@
+console.log('Hello Noelle')
 
-//my const var's
+//$(document).ready(function() {
 
+    //focus on first text field "Name"
+    $("#name").focus();
 
-const nameInput = document.getElementById("name");
-const emailInput = document.getElementById("email");
-const $shirtDesign = $("#design");
-const $shirtColor = $("#colors-js-puns")
-const $express = $('input[name="express"]');
-const $selectMethod = $('#select-Method');
-const $workShop = $('input[name="js-workshop"]');
-const $selectTheme = $('#select-theme');
-const $otherTitle = $('#other-title');
-const $jsLibraries = $('input[name="js-libraries"]');
-const $node = $('input[name="node"]');
-const $jsFrameworks = $('input[name="js-frameworks"]');
-const $expressWorkShop = $('input[name="express"]');
-const $workShop = $('input[name="node"]');
-const $paypal = $('#paypal');
-const $bitcoin = $('#bitcoin');
-const $creditCard = $('#credit-card')
+// in the job role section hiding the 'other' option that was added 
+    let inputOther = document.getElementById("other-title");
+    inputOther.style.display ="none";
 
-
-
-
-//focus by default.
-$('#name').focus();
-//is JS is disabled hide the text feild 
-$("#other-title").hide();
-
-//Name:
-function validateName() {
-    if($('name').prop('value')) {
-        return (false);
-    } else {
-        return (true);
-    }
-}
-
-/***********************************/
-            //Job Role//
-/************************************/
-//text will show when "other" is selected
-$('#title').on('click', function(e){   
-//if other is picked
-    if (e.target.value === 'other') {
-        $('#other').show();
-    } else {
-//if "other" is not picked
-        $('#other').hide();
-    }
-});
-/*************** */
-    //T-Shirt Info Section
-/*************** */
-//to show the color options that are in the design menu, if the user picks Js puns or 
-//I love JS in the menu it will only display what is picked
-$shirtColor.hide();
-$($shirtDesign).change(function(){
-    if ($shirtDesign.val() === "js puns"){
-        $shirtColor.show();
-        $('#color option[value="cornflowerblue"]').show();
-        $('#color option[value="darkslategray"]').show();
-        $('#color option[value="gold"]').show();
-        $('#color option[value="tomato"]').show();
-        $('#color option[value="steelblue"]').show();
-        $('#color option[value="dimgray"]').show();
-    } else {
-        if ($shirtDesign.val() === "heart js") {
-            $shirtDolor.show();
-            $('#color option[value="cornflowerblue"]').show();
-            $('#color option[value="darkslategray"]').show();
-            $('#color option[value="gold"]').show();
-            $('#color option[value="tomato"]').hide();
-            $('#color option[value="steelblue"]').hide();
-            $('#color option[value="dimgray"]').hide();
+// when the 'other' is selected show the text feild 
+    $("#title").change(function(e) { 
+        const selected = event.target.value;
+        if (selected == "other") {
+            $("#other-title").show();
+        } else { 
+            $("other-title").hide();
         }
-    }
-});
+    })
 
-//select theme is disabled 
-$(function() {
-    $selectTheme.prop("disabled", true);
-});
 
-//********************************** */
-   //Register for Activities section
-//********************************** */
-// If the user selects a workshop, don't allow selection of a workshop at the same day and time
-//disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
-//When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.
-//As a user selects activities, a running total should display below the list of checkboxes
-$workShop.change(function() {
-    if ($(this).is(':checked')) {
-        $express.prop('disabled', true);
+//T-Shirt
+//no options are shown when page loads
+$("#color")
+    .find("option")
+    .remove();
+
+    $("#color").append(
+        '<option value="shirt-menu">Please select a T-shirt theme</option>'
+      );
+      
+
+      //in the Design section, show only the color options that match
+      const toggleDropDown = () => {
+        $("#color")
+          .find("option")
+          .remove();
+        if ($("#design").val() == "js puns") {
+          $("#color").append(
+            '<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>'
+          );
+          $("#color").append(
+            '<option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option>'
+          );
+          $("#color").append(
+            '<option value="gold">Gold (JS Puns shirt only)</option>'
+          );
+        }
+        if ($("#design").val() == "heart js") {
+          $("#color").append(
+            '<option value="tomato">Tomato (I &#9829; JS shirt only)</option>'
+          );
+          $("#color").append(
+            '<option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option>'
+          );
+          $("#color").append(
+            '<option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>'
+          );
+        }
+      };
+      $("#design").change(toggleDropDown);
+     
+
+//Activity Section
+const jsFrameworks = $("input[name='js-frameworks'");
+const jsLibraries = $("input[name='js-libs']");
+const express = $("input[name='express']");
+const node = $("input[name='node']");
+var totalCost = 0;
+$('.activities').append('<div id="total"></div>');
+var updateCost = function (cost) {
+    totalCost += cost;
+    document.getElementById("total").innerHTML = "Total: $" + totalCost;
+};
+$("input[name='all']").change(function () {
+    if ($(this).prop("checked")) {
+        updateCost(200);
     } else {
-        $express.prop('disabled', false);
+        updateCost(-200);
     }
 });
 
-//work-shop js
-$jsFrameworks.change(function {
-    if ($(this).is(':checked')) {
-        $expressWorkShop.prob('disabled, true');
+//if jsFramework is picked, make the rest not work/show
+jsFrameworks.change(function () {
+    if ($(this).prop("checked")) {
+        express.prop("disabled", true);
+        express.parent().addClass("disabled");
+        express.parent().append()
+        updateCost(100);
     } else {
-        Work.prop('disabled', false);
+        express.prop("disabled", false);
+        express.parent().removeClass("disabled");
+        express.parent().find('.unavailable').remove();
+        updateCost(-100);
     }
 });
 
-$jsLibraries.change(function(){
-    if ($(this).is(':checked')) {
-        $node.prop('disabled', true);
+//if jsLibraries is picked, make the rest not work/show
+jsLibraries.change(function () {
+    if ($(this).prop("checked")) {
+        nodeJS.prop("disabled", true);
+        nodeJS.parent().addClass("disabled");
+        nodeJS.parent().append();
+        updateCost(100);
     } else {
-        $node.prop('disabled', false);
+        nodeJS.prop("disabled", false);
+        nodeJS.parent().removeClass("disabled");
+        nodeJS.parent().find('.unavailable').remove();
+        updateCost(-100);
     }
 });
 
-$expressWorkShop.change(function() {
-    if ($(this).is(':checked') {
-        $jsFrameworks.prop('disabled', true);
+// if express is picked make the rest not work/show
+express.change(function () {
+    if ($(this).prop("checked")) {
+        jsFrameworks.prop("disabled", true);
+        jsFrameworks.parent().addClass("disabled"); 
+        jsFrameworks.parent().append();
+        updateCost(100);
     } else {
-        $jsFrameworks.prop('disabled', false);
+        jsFrameworks.prop("disabled", false);
+        jsFrameworks.parent().removeClass("disabled");
+        jsFrameworks.parent().find('.unavailable').remove();
+        updateCost(-100);
     }
 });
 
-$node.change(function() {
-    if ($(this).is(':checked') {
-        $jsLibraries.prop('disabled', true);
+//if node is picked, make the rest not work/show
+node.change(function () {
+    if ($(this).prop("checked")) {
+        jsLibraries.prop("disabled", true);
+        jsLibraries.parent().addClass("disabled"); 
+        jsLibraries.parent().append();
+        updateCost(100);
     } else {
-        $jsLibraries.prop('disabled', false);
+        jsLibraries.prop("disabled", false);
+        jsLibraries.parent().removeClass("disabled");
+        jsLibraries.parent().find('.unavailable').remove();
+        updateCost(-100);
     }
 });
 
-
-//****************************** */
-         //Payment info
- //***************************** */
-
-//creditcard will be selected first
-$paymentOPtions[0].selectIndex = 1;
- //lets hide  paypal and bitcoin when loaded
- $paypal.hide();
- $bitcoin.hide();
-//Display payment sections based on the payment option chosen in the select menu.
-//When a user selects the "PayPal" payment option, the PayPal information should display, and the credit card and “Bitcoin” information should be hidden.
-//When a user selects the "Bitcoin" payment option, the Bitcoin information should display, and the credit card and “PayPal” information should be hidden.
-$(paymentOPtions).change(function() {
-    if ($(this).val() === "credit card") {
-        $creditCard.show();
+//
+$("input[name='build-tools']").change(function () {
+    if ($(this).prop("checked")) {
+        updateCost(100);
     } else {
-        $creditCard.hide();
-    }
-
-if ($(this).val() === "paypal") {
-    $paypal.show();
-} else {
-    $paypal.hide();
-}
-if ($(this).val() === "bitcoin") {
-    $bitcoin.show();
-} else {
-    $bitcoin.hide();
+        updateCost(-100);
     }
 });
-    
-$(function() {
-    $selectMethod.prop("disabled", true);
+$("input[name='npm']").change(function () {
+    if ($(this).prop("checked")) {
+        updateCost(100);
+    } else {
+        updateCost(-100);
+    }
 });
 
+//payment Info 
+//the credit-card is the default 
+$("#payment").ready(function() {
+    $('select option:contains("Credit Card")').prop("selected", true);
+  });
 
+  //paypal and bitcoin
+  const $paypal = $("#credit-card").next("div");
+  const $bitcon = $("#credit-card")
+    .next("div")
+    .next("div");
 
-// //Hiding Job Role
-// $('#title').on('change', function() {
-//     if ($('this').val() === 'other') {
-//         $('.other').show();
-//     } else {
-//         $('.other').hide();
-//     }
-// });
+    //hiding and showing 
+    $("#payment").change(function() {
+        if (this.value == "credit card") {
+          $("#credit-card").show();
+          $($paypal).hide();
 
-// $('design: first-child').hide();
-// $('#color-js-puns').hide();
-
-// /****************************** */
-// //T-Shirt section, hides color
-// /****************************** */
-// //hide shirts
-// $('#color').childern().hide();
-// $('#design').on('click', function(e){
-
-//   if($('#design').prop('value') === 'Select Theme') {
-//         $('#colors-js-puns').hide();
-//     }
-// hideColor();
-
-// //shows color once the select menu theme is picked
-// function showColor() {
-//     $('#colors-js-puns').show();
-//     $('#color option').remove();
-// };
-// $('#design').change(function(e) {
-// showColor();
-// })
-
-// //using switch to update color options to match the theme to the design
-// function showColor(theme) {
-//     const currentOptions = [];
-//     const colorDiv = document.querySelector('#colors-js-puns');
-
-//   for (let child of colorDiv.options) {
-//       switch (theme) {
-//           case selectTheme: {
-//             hideElement(child);
-//             hideElement(colorDiv);
-//             break;
-//           }
-//           //Theme for JS Puns shirts
-//           case "Theme - color-js-puns": {
-//               if (child.value === "cornflowerblue" || 
-//                   child.value === "darkslategray"  ||
-//                   child.value === "gold") {
-//                       showElement(child);
-//                       showElement(colorDiv);
-//                   } else { 
-//                       hideElement(child)
-
-//                   }
-//                  break;
-//           }
-//           //Theme for I<3 JS
-//           case "Theme - I Love JS": {
-//               if (child.value === "tomato" ||
-//                     child.value === "steelblue" ||
-//                     child.value === "dimgray") {
-//                     showElement(child);
-//                     showElement(colorDiv);
-//                 } else {
-//                     hideElement(child)
-//                 }
-//                 break;
-//           }
-//         //hide whatever isnt picked 
-//         default: {
-//     hideElement(child);
-//     hideElement(colorDiv);
-//      }
-//      break;
-//     }
-   
-//     if (!hideElement(child)) {
-//         currentOptions.push(child);
-//     }
-
-//     if (currentOptions.length > 0) {
-//         selected(currentOptions[0]);
-//     }
-// }
-
-// function hideElement(element) {
-//     element.style.display = "none";
-// }
-
-// function elementIsHidden(element) {
-//     if(element.style.display === "none"); {
-//         return (true);
-//     } else {
-//         return (false)
-// }
-
-// //register for Activities and the total cost
-// let totalCost = 0;
-// let price; 
-// price = $('<label>').addClass('total').text('Total: $ ' + totalCost); 
-// $('.activities').append(price);
-
-// //Listening for the changes in the Register for Activities section
-// $('[type="checkbox"]').on('change',function(e) {
-//     const checkSelection = $(e.target);
-//     const thetext = checkSelection.parent().text();
-//     const amountIndex = thetext.indexOf('$');
-//     const totalCost = theText.slice(amountIndex + 1);
-//     const numberCost = parseInt(addingCost);
-//     const dashIndex = theText.indexOf("-");
-//     const comaIndex = theText.indexOf(',');
-//     timeAndDate = theText.slice(dashIndex +1, comaIndex)
-//     //https://stackoverflow.com/questions/11440128/jquery-test-if-checkbox-is-not-checked
-//     if (checkSelection.is(':checked')) {
-//         totalCost += numberCost;
-//         $('.total').text('Total: $ ' + totalCost);
-//     } else {
-//         totalCost -= numberCost;
-//         $('.total').text('Total: $ ' + toatlCost);
-//     }
-// })
-
-// //disable activities thats are conflicting with the other choices 
-// const inputCheck = $("input[type='checkbox']"); 
-//     for (let i = 0; i < inputCheck.length; i++) {
-//         const checkboxItem = inputCheck.eq(i).parent().text();
-//     if(checkboxItem.includes(timeAndDate) && checkboxItem !== theText) {
-//         if(checkSelection.is(':checked')) {
-//             inputCheck.eq(i).attr("disable", true);
-//             inputCheck.eq(i).parent().css("text decoration", "line through");
-//         } else{
-//             inputCheck.eq(i).attr("disabled", false);
-//             inputCheck.eq(i).parent().css("text decoration", "line through");
-//         }
-//     }
-//     });
-
-//     /********************************* */
-//                //register
+    //input can not be blank
+    $("input[name=user_name]").on("input", function(event) {
+        nameValidation();
+      });
+     
+      function nameValidation() {
+        const name = $("#name").val();
+        if (name.length < 1) {
+          $("#name")
+            .after('<span class="error-message">This field is required')
+            .addClass("invalid");
+          $(".error-message").addClass("error-text");
+        } else {
+          $("#name")
+            .addClass("valid")
+            .removeClass("invalid");
+          $(".error-message").remove();
+        }
+      }
 
 
